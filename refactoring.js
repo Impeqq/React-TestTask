4fc94a23f571238c398e9c730719fc58f043fc5d
Impeqq <sokovnin.m@list.ru>
2020-08-15 20:02:48 +0300

    add 1 question

---
+++ refactoring.js
@@ -0,0 +1,20 @@
+function func(s, a, b) {
+  if (s.match(/^$/)) {
+    return -1;
+  }
+  var i = s.length - 1,
+    aIndex = -1,
+    bIndex = -1;
+  while (aIndex == -1 && bIndex == -1 && i > 0) {
+    if (s[i] == a) {
+      aIndex = i;
+      return aIndex;
+    }
+    if (s[i] == b) {
+      bIndex = i;
+      return bIndex;
+    }
+    i--;
+  }
+  return -1;
+}
